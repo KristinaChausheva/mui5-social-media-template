32720e75f148f1c545283f17428afbdcecb29f18
Kristina Chausheva <k.chausheva@gmail.com>
2022-04-19 23:45:57 +0300

    creating components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Button, Fab, styled } from "@mui/material"
+import DownloadIcon from "@mui/icons-material/Download"
+import AddIcon from "@mui/icons-material/Add"
+
+import Sidebar from "./components/Sidebar"
+import Feed from "./components/Feed"
+import Rightbar from "./components/Rightbar"
 function App() {
-  return <div className="App"></div>
+  return (
+    <div>
+      <Sidebar />
+      <Feed />
+      <Rightbar />
+    </div>
+  )
 }
 
 export default App
